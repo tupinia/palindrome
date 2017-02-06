dd9051c972c5e46e34093b212670bd7ce4396335
tupinia <mathieu.chaudron@gmail.com>
2017-02-06 18:29:50 +0000

    change test frow vagrant

--- ecobee-palindrome/src/test/java/PalindromicNumberDemoTest.java
+++ ecobee-palindrome/src/test/java/PalindromicNumberDemoTest.java
@@ -4,7 +4,7 @@ public class PalindromicNumberDemoTest extends TestCase {
 
 	public void testIsPalindrome() {
 		
-		assertTrue(PalindromicNumberDemo.isPalindrome("1"));
+		assertTrue(PalindromicNumberDemo.isPalindrome("8"));
 		assertFalse(PalindromicNumberDemo.isPalindrome("123"));
 		assertTrue(PalindromicNumberDemo.isPalindrome("100010001"));
 	}
