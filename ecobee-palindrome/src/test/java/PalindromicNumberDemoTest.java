176bbeb65d4bedf056cbea2ba0db9370c27811d0
Tupinia <Tupinia@NZXT-i7>
2017-02-06 13:31:46 -0500

    commit from vaio

--- ecobee-palindrome/src/test/java/PalindromicNumberDemoTest.java
+++ ecobee-palindrome/src/test/java/PalindromicNumberDemoTest.java
@@ -4,8 +4,9 @@ public class PalindromicNumberDemoTest extends TestCase {
 
 	public void testIsPalindrome() {
 		
-		assertTrue(PalindromicNumberDemo.isPalindrome("1"));
+		assertTrue(PalindromicNumberDemo.isPalindrome("0"));
 		assertFalse(PalindromicNumberDemo.isPalindrome("123"));
 		assertTrue(PalindromicNumberDemo.isPalindrome("100010001"));
+		assertFalse(PalindromicNumberDemo.isPalindrome("10001000100"));
 	}
 }
