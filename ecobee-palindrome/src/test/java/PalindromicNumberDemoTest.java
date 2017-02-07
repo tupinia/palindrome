350fea114a182b444a168f447b259fa55bb978f9
Tupinia <Tupinia@NZXT-i7>
2017-02-07 09:50:32 -0500

    Add more test and comment

--- ecobee-palindrome/src/test/java/PalindromicNumberDemoTest.java
+++ ecobee-palindrome/src/test/java/PalindromicNumberDemoTest.java
@@ -2,6 +2,9 @@ import junit.framework.TestCase;
 
 public class PalindromicNumberDemoTest extends TestCase {
 
+	/**
+	 * Test unit for palindrome
+	 */
 	public void testIsPalindrome() {
 		
 		assertTrue(PalindromicNumberDemo.isPalindrome("0"));
@@ -10,7 +13,7 @@ public class PalindromicNumberDemoTest extends TestCase {
 		assertFalse(PalindromicNumberDemo.isPalindrome("10001000100"));
 
 		
-		assertTrue(PalindromicNumberDemo.isPalindrome("12321"));
+		assertTrue(PalindromicNumberDemo.isPalindrome("22322"));
 		assertFalse(PalindromicNumberDemo.isPalindrome("7777777777777754"));
 	}
 }
