a74aa744e2d4b6d35f249fc88501ce63c6258c74
tupinia <mathieu.chaudron@gmail.com>
2017-02-07 14:47:18 +0000

    Add more tests

--- ecobee-palindrome/src/test/java/PalindromicNumberDemoTest.java
+++ ecobee-palindrome/src/test/java/PalindromicNumberDemoTest.java
@@ -9,8 +9,9 @@ public class PalindromicNumberDemoTest extends TestCase {
 		assertTrue(PalindromicNumberDemo.isPalindrome("100010001"));
 		assertFalse(PalindromicNumberDemo.isPalindrome("10001000100"));
 
-		
 		assertTrue(PalindromicNumberDemo.isPalindrome("12321"));
 		assertFalse(PalindromicNumberDemo.isPalindrome("7777777777777754"));
+		
+		assertFalse(PalindromicNumberDemo.isPalindrome("77777778877754"));
 	}
 }
